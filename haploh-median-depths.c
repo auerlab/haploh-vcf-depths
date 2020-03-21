a7a130c1eb879ba5eb01c23341ca1c2d3a300c37
Jason Bacon <bacon@compute-006.peregrine>
2020-03-21 08:21:54 -0500

    Frame out program and Makefile

---
+++ haploh-median-depths.c
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *  Description:
+ *      Rapidly compute median depths of VCF files for haplohseq events
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2020-03-21  Jason Bacon Begin
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <sysexits.h>
+#include <vcfio.h>
+
+int     main(int argc,char *argv[])
+
+{
+    return EX_OK;
+}
+
