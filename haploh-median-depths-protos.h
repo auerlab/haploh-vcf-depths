179ebbf5d5b6e3c4be30862d70dd342a634a75bb
Jason Bacon <bacon@orca.acadix.biz>
2020-03-22 11:39:48 -0500

    Flesh out main loop and add read_event()

--- haploh-median-depths-protos.h
+++ haploh-median-depths-protos.h
@@ -1,4 +1,5 @@
 /* haploh-median-depths.c */
 int main(int argc, const char *argv[]);
 void usage(const char *argv[]);
-int haploh_median_depths(FILE *events_stream, const char *vcf_dir);
+int haploh_median_depths(const char *argv[], FILE *events_stream, const char *vcf_dir);
+int read_event(FILE *event_stream, event_t *event);
--- haploh-median-depths.c
+++ haploh-median-depths.c
@@ -14,25 +14,26 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <vcfio.h>
+#include <tsvio.h>
 #include "haploh-median-depths.h"
 
 int     main(int argc,const char *argv[])
 
 {
-    FILE        *events_stream;
-    const char  *events_file,
+    FILE        *event_stream;
+    const char  *event_file,
 		*vcf_dir;
     struct stat dir_stat;
     
-    if ( argc != 2 )
+    if ( argc != 3 )
 	usage(argv);
 
-    events_file = argv[1];
+    event_file = argv[1];
     vcf_dir = argv[2];
     
-    if ( (events_stream = fopen(events_file, "r")) == NULL )
+    if ( (event_stream = fopen(event_file, "r")) == NULL )
     {
-	fprintf(stderr, "Cannot open %s: %s\n", events_file, strerror(errno));
+	fprintf(stderr, "Cannot open %s: %s\n", event_file, strerror(errno));
 	exit(EX_NOINPUT);
     }
     
@@ -44,7 +45,7 @@ int     main(int argc,const char *argv[])
 	exit(EX_NOINPUT);
     }
 	    
-    return haploh_median_depths(events_stream, vcf_dir);
+    return haploh_median_depths(argv, event_stream, vcf_dir);
 }
 
 
@@ -56,8 +57,93 @@ void    usage(const char *argv[])
 }
 
 
-int     haploh_median_depths(FILE *events_stream, const char *vcf_dir)
+int     haploh_median_depths(const char *argv[], FILE *event_stream,
+			     const char *vcf_dir)
 
 {
-    return EX_OK;
+    int     separator;
+    event_t event;
+    
+    while ( ((separator = read_event(event_stream, &event)) == EVENT_READ_OK)
+	    || (separator == EVENT_READ_HEADER) )
+    {
+	fprintf(stderr, "%s %zu %zu\n", event.chromosome, event.begin, event.end);
+	/*
+	 *  Compute median depth of VCF calls between event.begin and event.end
+	 *  for this sample.
+	 */
+	
+	/*
+	 *  Compute median depth of VCF calls between event.begin and event.end
+	 *  for all other samples.
+	 */
+	// Use glob()?
+    }
+    
+    if ( separator == EOF )
+	return EX_OK;
+    else
+	return EX_DATAERR;
+}
+
+
+int     read_event(FILE *event_stream, event_t *event)
+
+{
+    int     separator,
+	    status;
+    size_t  len;
+    char    temp[VCF_POSITION_MAX_CHARS + 1],
+	    *endptr;
+    
+    separator = tsv_read_field(event_stream, event->chromosome,
+			       VCF_CHROMOSOME_MAX_CHARS, &len);
+    if ( separator == '\t' )
+    {
+	fprintf(stderr, "chromosome = %s\n", event->chromosome);
+	
+	// BEGIN
+	if ( (separator = tsv_read_field(event_stream, temp,
+			       VCF_POSITION_MAX_CHARS, &len)) != '\t' )
+	{
+	    fputs("read_event(): Did not find tab after BEGIN.\n", stderr);
+	    return EVENT_READ_TRUNCATED;
+	}
+	if ( strcmp(temp, "BEGIN") == 0 )
+	{
+	    status = EVENT_READ_HEADER;
+	}
+	else
+	{
+	    event->begin = strtoul(temp, &endptr, 10);
+	    if ( *endptr != '\0' )
+	    {
+		fprintf(stderr, "read_event(): Invalid BEGIN: %s\n", temp);
+		return EVENT_READ_TRUNCATED;
+	    }
+	    
+	    // END
+	    if ( (separator = tsv_read_field(event_stream, temp,
+				   VCF_POSITION_MAX_CHARS, &len)) != '\t' )
+	    {
+		fputs("read_event(): Did not find tab after END.\n", stderr);
+		return EVENT_READ_TRUNCATED;
+	    }
+	    event->end = strtoul(temp, &endptr, 10);
+	    if ( *endptr != '\0' )
+	    {
+		fprintf(stderr, "read_event(): Invalid END: %s\n", temp);
+		return EVENT_READ_TRUNCATED;
+	    }
+	    status = EVENT_READ_OK;
+	}
+	if ( (separator = tsv_skip_rest_of_line(event_stream)) != '\n' )
+	{
+	    fprintf(stderr, "read_event(): Did not find newline at end of event.\n");
+	    return EVENT_READ_TRUNCATED;
+	}
+	return status;
+    }
+    fprintf(stderr, "separator = %d\n", separator);
+    return separator;
 }
