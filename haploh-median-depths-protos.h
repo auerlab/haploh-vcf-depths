f8f5bc5f33e15b9df956ecfc69f66ed675d2f40e
Jason Bacon <bacon@compute-006.peregrine>
2020-03-21 20:17:16 -0500

    Process command line args and stub out top-level function

--- haploh-median-depths-protos.h
+++ haploh-median-depths-protos.h
@@ -1,3 +1,4 @@
 /* haploh-median-depths.c */
 int main(int argc, const char *argv[]);
 void usage(const char *argv[]);
+int haploh_median_depths(FILE *events_stream, const char *vcf_dir);
--- haploh-median-depths.c
+++ haploh-median-depths.c
@@ -10,21 +10,54 @@
 #include <stdio.h>
 #include <sysexits.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <vcfio.h>
 #include "haploh-median-depths.h"
 
 int     main(int argc,const char *argv[])
 
 {
-    if ( argc != 1 )
+    FILE        *events_stream;
+    const char  *events_file,
+		*vcf_dir;
+    struct stat dir_stat;
+    
+    if ( argc != 2 )
 	usage(argv);
-    return EX_OK;
+
+    events_file = argv[1];
+    vcf_dir = argv[2];
+    
+    if ( (events_stream = fopen(events_file, "r")) == NULL )
+    {
+	fprintf(stderr, "Cannot open %s: %s\n", events_file, strerror(errno));
+	exit(EX_NOINPUT);
+    }
+    
+    if ( (stat(vcf_dir, &dir_stat) == -1) || !S_ISDIR(dir_stat.st_mode) )
+    {
+	fprintf(stderr,
+		"%s must be a readable directory containing VCF files.\n",
+		vcf_dir);
+	exit(EX_NOINPUT);
+    }
+	    
+    return haploh_median_depths(events_stream, vcf_dir);
 }
 
 
 void    usage(const char *argv[])
 
 {
-    fprintf(stderr, "Usage: %s haplohseq-event-file\n", argv[0]);
+    fprintf(stderr, "Usage: %s haplohseq-event-file directory-with-VCF-files\n", argv[0]);
     exit(EX_USAGE);
+}
+
+
+int     haploh_median_depths(FILE *events_stream, const char *vcf_dir)
+
+{
+    return EX_OK;
 }
