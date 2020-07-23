e62ddd99852250207fbbd36d7cacacdcd1c9f3d9
Jason Bacon <jwb@FreeBSD.org>
2020-07-23 15:26:49 -0500

    Update for new vcf API

--- haploh-vcf-depths.c
+++ haploh-vcf-depths.c
@@ -107,7 +107,7 @@ int     haploh_median_depths(const char *event_glob_pattern,
 	// Count calls for same chromosome and within range
 	while ( (status = vcf_read_ss_call(vcf_stream, &vcf_call,
 			      VCF_SAMPLE_MAX_CHARS)) ==
-			      VCF_READ_OK )
+			      VCF_OK )
 	{
 	    if ( (depth_str = strrchr(vcf_sample, ':')) == NULL )
 	    {
